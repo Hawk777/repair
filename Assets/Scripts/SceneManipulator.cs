1645745ddd9226ce7437ee4b52cc16bb6830d89c
Zereijin <zereijin@gmail.com>
2020-02-01 11:54:52 -0800

    Title screen and scene manager

---
+++ Assets/Scripts/SceneManipulator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManipulator : MonoBehaviour
+{
+	[SerializeField] float gameOverDelayInSeconds = 2f;
+
+	public void LoadStartMenu() {
+		SceneManager.LoadScene(0);
+	}
+
+	public void LoadNextLevel() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void LoadGameOver() {
+		StartCoroutine(WaitAndLoad());
+	}
+
+	IEnumerator WaitAndLoad() {
+		yield return new WaitForSeconds(gameOverDelayInSeconds);
+		SceneManager.LoadScene("Game Over");
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
