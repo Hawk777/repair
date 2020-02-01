c9d63f3ed201ba20b17dcf5780ecfa022a626b8d
Alexander Gagne <schrodingerfu314@gmail.com>
2020-02-01 14:31:42 -0800

    torque-based car

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody2D enemyBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyBody = GetComponentInParent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the player is too far away, don't do anything.  If they are close enough to render, attack the nearest thing that can be attacked (either player or objectives
+        GameObject player = GameManager.getPlayer();
+
+        // TODO
+        
+    }
+}
