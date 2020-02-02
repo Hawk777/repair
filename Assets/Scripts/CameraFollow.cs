4031f3967f3cbd1a465e273c317e0a8b052fda4f
Alexander Gagne <schrodingerfu314@gmail.com>
2020-02-02 14:50:16 -0800

    camera correctly stops following on start/end screen

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     public float damper = 0.1f;
     private Transform playerTransform;
+    private Vector3 defaultPosition = new Vector3(0,0,-10);
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
@@ -16,10 +18,16 @@ public class CameraFollow : MonoBehaviour
     {
         //transform.position = new Vector3(playerTransform.position.x,playerTransform.position.y,transform.position.z);
         //transform.rotation = playerTransform.rotation * Quaternion.Euler(0, 0, 0);
-
-        Vector2 temp = Vector2.Lerp(transform.position, playerTransform.position, damper);
-        transform.position = new Vector3(temp.x, temp.y, transform.position.z);
-        transform.rotation = Quaternion.Lerp(transform.rotation, playerTransform.rotation*Quaternion.Euler(0,0,0), damper);
-
+        if (SceneManager.GetActiveScene().name != "Start Menu" && SceneManager.GetActiveScene().name != "Game Over")
+        {
+            Vector2 temp = Vector2.Lerp(transform.position, playerTransform.position, damper);
+            transform.position = new Vector3(temp.x, temp.y, transform.position.z);
+            transform.rotation = Quaternion.Lerp(transform.rotation, playerTransform.rotation * Quaternion.Euler(0, 0, 0), damper);
+        }
+        else
+        {
+            transform.rotation = Quaternion.identity;
+            transform.position = defaultPosition;
+        }
     }
 }
